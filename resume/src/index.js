6c8f3d5a7d5cc2d015873c65b8adc000fb3bb6bd
Kabir Grewal <kgrewal7777@gmail.com>
2021-05-20 22:00:02 -0600

    adding mobile-first css and creating navbar, left nav collapsible menu

--- resume/src/index.js
+++ resume/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// import Navbar from 'react-bootstrap/Navbar'
+
 import keyboard from './img/keyboard.jpg';
 import resume from './docs/resume.pdf';
 import linkedinIcon from './img/linkedin.png'
@@ -9,38 +11,37 @@ import downloadIcon from './img/download.png'
 import githubIcon from './img/github.png'
 
 
+
 const aboutMe = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 const email = "kgrewal7777@gmail.com";
 const linkedin = "https://www.linkedin.com/in/kabir-grewal-9b8b8a10a";
 const github = "https://github.com/kgrewal77";
 
-class TitleLabel extends React.Component {
+// class TitleLabel extends React.Component {
 
-  render() {
+//   render() {
 
-    return (
-      <text className={"title-label "+this.props.classes}>
-        {this.props.text}
-      </text>
-    );
+//     return (
+//       <div className={"title-label "+this.props.classes}>
+//         {this.props.text}
+//       </div>
+//     );
+//   }
+// }
+
+function toggleLeftNav() {
+
+    let ln = document.getElementsByClassName("leftnav")[0];
+    //console.log(document.getElementById("content"));
+    if (ln.style.width === "25vw") {
+      ln.style.width = "0";
+      //document.getElementById("content").style.marginLeft = "0px";
+    } else {
+      ln.style.width = "25vw";
+      //document.getElementById("content").style.marginLeft = "25vw";
+    }
   }
-}
 
-
-class DownloadBtn extends React.Component {
-
-  render() {
-
-    return (
-      <a href={this.props.link}
-         target="_blank" > 
-        <button className={"download-btn "+this.props.classes}>
-          {this.props.text}
-        </button>
-      </a>
-    );
-  }
-}
 
 class LinkImage extends React.Component {
 
@@ -48,7 +49,8 @@ class LinkImage extends React.Component {
 
     return (
       <a href={this.props.link}
-          target="_blank" > 
+          target="_blank"
+          rel="noreferrer" > 
         <img src={this.props.image_src}
              alt={this.props.image_alt}
              className={this.props.classes}>
@@ -75,6 +77,7 @@ class ImgPane extends React.Component {
 
 class TextPane extends React.Component {
 
+
   render() {
 
     return (
@@ -87,47 +90,72 @@ class TextPane extends React.Component {
   }
 }
 
+class Header extends React.Component {
+
+  
+
+  render(){
+     return (
+      <div>
+        <div className="leftnav"><a href="#aboutMe">About Me</a></div>
+        <div className="header sticky">
+
+            <i className="navicon fa fa-bars" onClick={toggleLeftNav}></i>
+
+        </div>
+      </div>
+        );  
+   }
+}
+
 class AboutMe extends React.Component {
   render() {
     return (
-      <div className="slide">
-        <ImgPane image_src={keyboard} 
-                 image_alt="muted neutrals keyboard">
-          <span className="center-of-pane"
-                style={{transform: 'translate(-50%, -20%)',
-                        top:'20%'}}>
-            <TitleLabel text={"Kabir Grewal"} classes="resume-title-label"/>
-          </span>
-          <span className="center-of-pane"
-               style={{transform: 'translate(-50%, -90%)',
-                        top:'90%'}}>            
-            <LinkImage image_src={downloadIcon}
-                       image_alt="download logo"
-                       link={resume}
-                       classes="fourth"/>
-            <LinkImage image_src={emailIcon}
-                       image_alt="gmail logo"
-                       link={`mailto:${email}`}
-                       classes="fourth"/>
-            <LinkImage image_src={githubIcon}
-                       image_alt="github logo"
-                       link={github}
-                       classes="fourth"/>
-            <LinkImage image_src={linkedinIcon}
-                       image_alt="linkedin logo"
-                       link={linkedin}
-                       classes="fourth"/>
-          </span>
-        </ImgPane>
-        <TextPane text={aboutMe}/>
-      </div>
+        <div id={this.props.id} className="slide top-slide">
+          <TextPane text={aboutMe}/>
+          <ImgPane image_src={keyboard} 
+                   image_alt="muted neutrals keyboard">
+            <span className="center-of-pane">            
+              <LinkImage image_src={downloadIcon}
+                         image_alt="download logo"
+                         link={resume}
+                         classes="fourth"/>
+              <LinkImage image_src={emailIcon}
+                         image_alt="gmail logo"
+                         link={`mailto:${email}`}
+                         classes="fourth"/>
+              <LinkImage image_src={githubIcon}
+                         image_alt="github logo"
+                         link={github}
+                         classes="fourth"/>
+              <LinkImage image_src={linkedinIcon}
+                         image_alt="linkedin logo"
+                         link={linkedin}
+                         classes="fourth"/>
+            </span>
+          </ImgPane>
+        </div>
+        
     );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <div id="content">
+          <AboutMe id="aboutMe"/>
+          <AboutMe/>
+        </div>
+      </div>);
   }
 }
 
 // ========================================
 
 ReactDOM.render(
-  <AboutMe />,
+  <App />,
   document.getElementById('root')
 );
