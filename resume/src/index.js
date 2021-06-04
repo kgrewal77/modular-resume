7c438c7d79c7feb1a5d2fbf2bfb2fcdf0547fe7d
Kabir Grewal <kgrewal7777@gmail.com>
2021-06-04 16:27:46 -0600

    changing linkedin logo, removing menu, and making icon bar collapsible

--- resume/src/index.js
+++ resume/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
 import keyboard from './img/keyboard.jpg';
 import resume from './docs/resume.pdf';
-import linkedinIcon from './img/linkedin.png'
+import linkedinIcon from './img/linkedin3.jpg'
 import emailIcon from './img/email.png'
 import downloadIcon from './img/download.png'
 import githubIcon from './img/github.png'
@@ -28,20 +28,6 @@ const github = "https://github.com/kgrewal77";
 //     );
 //   }
 // }
-
-function toggleLeftNav() {
-
-    let ln = document.getElementsByClassName("leftnav")[0];
-    //console.log(document.getElementById("content"));
-    if (ln.style.width === "25vw") {
-      ln.style.width = "0";
-      //document.getElementById("content").style.marginLeft = "0px";
-    } else {
-      ln.style.width = "25vw";
-      //document.getElementById("content").style.marginLeft = "25vw";
-    }
-  }
-
 
 class LinkImage extends React.Component {
 
@@ -90,23 +76,77 @@ class TextPane extends React.Component {
   }
 }
 
+class HeaderLink extends React.Component {
+
+  /*clickNavLink = () => {
+
+    console.log(document.getElementById(this.props.target))
+    if (document.getElementById(this.props.target)) {
+      console.log("scroll to id");
+      document.getElementById(this.props.target).scrollIntoView();
+
+        window.scrollBy(0,-window.innerHeight*.10);
+    }
+      this.props.parentCallback();
+  }*/
+
+  render () {
+
+    return (
+          <tr><td><div /*onClick={this.clickNavLink}*/>{this.props.text}</div></td></tr>
+      );
+  }
+}
+
 class Header extends React.Component {
 
-  
+  /*toggleNavBar = () => {
+
+    let ln = document.getElementsByClassName("topnav")[0];
+    //console.log(document.getElementById("content"));
+    if (ln.style.width === "100vw") {
+      ln.style.width = "0";
+      document.getElementsByClassName("header")[0].style.backgroundColor = "darkseagreen";
+    } else {
+      ln.style.width = "100vw";
+      document.getElementsByClassName("header")[0].style.backgroundColor = "peachpuff";
+    }
+  }*/
+
 
   render(){
      return (
       <div>
-        <div className="leftnav"><a href="#aboutMe">About Me</a></div>
+        <div className="topnav">
+{/*        <table className="link-table">
+          <HeaderLink
+            //onClick={this.clickNavLink}
+            text="About Me"
+            //parentCallback={this.toggleNavBar}
+            target="aboutMe"
+          ></HeaderLink>
+          <HeaderLink
+            //onClick={this.clickNavLink}
+            text="My Skills"
+            //parentCallback={this.toggleNavBar}
+            target="mySkills"
+          ></HeaderLink>
+          <HeaderLink
+            //onClick={this.clickNavLink}
+            text="Work Experience"
+            //parentCallback={this.toggleNavBar}
+            target="workExperience"
+          ></HeaderLink>        </table>*/}
+        </div>
         <div className="header sticky">
-
-            <i className="navicon fa fa-bars" onClick={toggleLeftNav}></i>
 
         </div>
       </div>
         );  
    }
 }
+
+
 
 class AboutMe extends React.Component {
   render() {
@@ -115,11 +155,77 @@ class AboutMe extends React.Component {
           <TextPane text={aboutMe}/>
           <ImgPane image_src={keyboard} 
                    image_alt="muted neutrals keyboard">
-            <span className="center-of-pane">            
+            
+          </ImgPane>
+        </div>
+        
+    );
+  }
+}
+
+class MySkills extends React.Component {
+  render() {
+    return (
+        <div id={this.props.id} className="slide">
+          <TextPane text={aboutMe}/>
+          <TextPane text={aboutMe}/>
+        </div>
+        
+    );
+  }
+}
+
+class WorkExperience extends React.Component {
+  render() {
+    return (
+        <div id={this.props.id} className="slide">
+          <TextPane text={aboutMe}/>
+          <TextPane text={aboutMe}/>
+        </div>
+        
+    );
+  }
+}
+
+class IconFooter extends React.Component {
+
+  constructor(props) {
+
+    super(props);
+    this.state = {
+      expanded: false
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.handleClickOutside, true);
+    document.addEventListener('touchstart', this.handleClickOutside, true);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleClickOutside, true);
+    document.removeEventListener('touchstart', this.handleClickOutside, true);
+  }
+
+  handleClickOutside = event => {
+      const domNode = ReactDOM.findDOMNode(this);
+
+      if ((!domNode || !domNode.contains(event.target)) && this.state.expanded) {
+          this.setState({
+              expanded: false
+          });
+      }
+  }
+
+  render() {
+    return (
+      <div>
+      {this.state.expanded
+        ? <div className="icon-footer">  
               <LinkImage image_src={downloadIcon}
                          image_alt="download logo"
                          link={resume}
-                         classes="fourth"/>
+                         classes="fourth-first fourth"/>
               <LinkImage image_src={emailIcon}
                          image_alt="gmail logo"
                          link={`mailto:${email}`}
@@ -132,10 +238,15 @@ class AboutMe extends React.Component {
                          image_alt="linkedin logo"
                          link={linkedin}
                          classes="fourth"/>
-            </span>
-          </ImgPane>
-        </div>
-        
+          </div>
+        : <div className="icon-footer">
+               <i className="expand-icon fa fa-arrow-circle-left  "
+                  onClick={()=>{this.setState({expanded:true});}}
+                  >
+               </i>
+          </div>}
+      </div>
+
     );
   }
 }
@@ -146,9 +257,11 @@ class App extends React.Component {
       <div>
         <Header/>
         <div id="content">
-          <AboutMe id="aboutMe"/>
-          <AboutMe/>
+         <AboutMe id="aboutMe"/>
+         <MySkills id="mySkills"/>
+         <WorkExperience id="workExperience"/>
         </div>
+        <IconFooter/>
       </div>);
   }
 }
