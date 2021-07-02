53d7d44836839df2bb0ed397461aa9201450c88f
Kabir Grewal <kgrewal7777@gmail.com>
2021-07-02 15:33:39 -0600

    cleaning up from prior commit

--- server/controllers/structure-ctrl.js
+++ server/controllers/structure-ctrl.js
@@ -71,7 +71,7 @@ updateStructure = async (req, res) => {
             .then(() => {
                 return res.status(200).json({
                     success: true,
-                    id: structure._id,
+                    key: structure.key,
                     message: 'Structure updated!',
                 })
             })
@@ -85,7 +85,7 @@ updateStructure = async (req, res) => {
 }
 
 deleteStructure = async (req, res) => {
-    await Structure.findOneAndDelete({ _id: req.params.id }, (err, structure) => {
+    await Structure.findOneAndDelete({ key: req.params.key }, (err, structure) => {
         if (err) {
             return res.status(400).json({ success: false, error: err })
         }
